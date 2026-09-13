e99dcc27850ca32fa8ef9ac41d1a81a80a0d564e
Mirza Yasir Abdullah Baig <yasirabdullah4549@gmail.com>
2026-09-13 23:06:17 +0500

    Delete services directory

--- services/__init__.py
+++
@@ -1,6 +0,0 @@
-from .stt_service import STTService
-from .claim_extractor import ClaimExtractor
-from .search_service import SearchService
-from .fact_checker import FactChecker
-
-__all__ = ["STTService", "ClaimExtractor", "SearchService", "FactChecker"]
