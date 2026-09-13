c51e49ce6cbd9f532c53301d87ba354fec8bee7a
Mirza Yasir Abdullah Baig <yasirabdullah4549@gmail.com>
2026-09-13 20:32:22 +0500

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -7,4 +7,4 @@ from .claim_extractor import ClaimExtractor
 from .search_service import SearchService
 from .fact_checker import FactChecker
 
-_all_ = ["STTService", "ClaimExtractor", "SearchService", "FactChecker"]
+all = ["STTService", "ClaimExtractor", "SearchService", "FactChecker"]
