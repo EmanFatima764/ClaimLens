9055da352de74824455393797dac3cf4403becac
Mirza Yasir Abdullah Baig <yasirabdullah4549@gmail.com>
2026-09-13 20:31:32 +0500

    README File Updated

--- services/__init__.py
+++ services/__init__.py
@@ -1,6 +1,10 @@
+"""
+services package.
+"""
+
 from .stt_service import STTService
 from .claim_extractor import ClaimExtractor
 from .search_service import SearchService
 from .fact_checker import FactChecker
 
-__all__ = ["STTService", "ClaimExtractor", "SearchService", "FactChecker"]
+_all_ = ["STTService", "ClaimExtractor", "SearchService", "FactChecker"]
